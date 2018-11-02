7ec92140a93f5ef4aec96006e8414e11c172b5de
Sergio Flores <relfos@gmail.com>
2018-11-02 20:50:20 +0000

    Initial version

---
+++ LunarFonts/EdgeList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LunarLabs.Fonts
+{
+    internal struct Edge
+    {
+        public float x0;
+        public float y0;
+        public float x1;
+        public float y1;
+        public bool invert;
+    }
+
+    internal class EdgeList
+    {
+        private List<Edge> edges = new List<Edge>(16);
+
+        public int Count => edges.Count;
+
+        public int Add(Edge item)
+        {
+            var result = edges.Count;
+            edges.Add(item);
+            return result;
+        }
+
+
+        public Edge Get(int index)
+        {
+            if (index < 0 || index >= edges.Count)
+            {
+                throw new Exception("Invalid edge index");
+            }
+
+            return edges[index];
+        }
+
+        private int EdgeCompare(Edge pa, Edge pb)
+        {
+            if (pa.y0 < pb.y0)
+                return -1;
+
+            if (pa.y0 > pb.y0)
+                return 1;
+
+            return 0;
+        }
+
+        private void QuickSort(int left, int right)
+        {
+            int i, j;
+            do
+            {
+                i = left;
+                j = right;
+                var pivot = edges[(left + right) / 2];
+                do
+                {
+
+                    while (EdgeCompare(edges[i], pivot) < 0)
+                    {
+                        i++;
+                    }
+
+                    while (EdgeCompare(edges[j], pivot) > 0)
+                    {
+                        j--;
+                    }
+
+                    if (i <= j)
+                    {
+                        var temp = edges[i];
+                        edges[i] = edges[j];
+                        edges[j] = temp;
+                        i++;
+                        j--;
+                    }
+
+                } while (i <= j);
+
+                if (left < j)
+                {
+                    QuickSort(left, j);
+
+                }
+
+                left = i;
+            } while (i < right);
+        }
+
+        public void Sort()
+        {
+            if (edges.Count > 1)
+            {
+                QuickSort(0, edges.Count - 1);
+            }
+        }
+
+        public void Fix(int index, float Y0)
+        {
+            var temp = edges[index];
+            temp.y0 = Y0;
+            edges[index] = temp;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Lunar Fonts
+Font loader / rasterizer for TTF format in pure C#.
+
+This code is a direct port from the Delphi version which I ported some years ago from C. 
+The [original C version](https://github.com/nothings/stb/blob/master/stb_truetype.h) was written by [Sean Barrett](http://nothings.org/)
+
+## Installation
+
+    PM> Install-Package LunarLabs.Fonts
+
+Since this is a .NET standard package, to use with .NET framework projects please set the target to .NET Framework 4.5 or higher, otherwise Nuget will give you installation errors.
+
+# Contact
+
+Let me know if you find bugs or if you have suggestions to improve the code.
+
+And maybe follow me [@onihunters](https://twitter.com/onihunters) :)
