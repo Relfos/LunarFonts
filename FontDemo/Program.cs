c42728b411de0db427d269a1bdcbdddb33d72269
Sergio Flores <relfos@gmail.com>
2019-01-16 01:22:22 +0000

    Demo now renders whole string instead of just a single char

--- FontDemo/Program.cs
+++ FontDemo/Program.cs
@@ -1,13 +1,15 @@
 ﻿using LunarLabs.Fonts;
 using System;
 using System.IO;
-using System.DrawingCore;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace FontDemo
 {
     class Program
     {
-        static void ExportPNG(PixelTarget image, string outputName)
+        static Bitmap ConvertToBitmap(GlyphBitmap image)
         {
             var bmp = new Bitmap(image.Width, image.Height);
             for (int j = 0; j < image.Height; j++)
@@ -15,25 +17,89 @@ namespace FontDemo
                 for (int i = 0; i < image.Width; i++)
                 {
                     byte alpha = image.Pixels[i + j * image.Width];
-                    bmp.SetPixel(i, j, Color.FromArgb(alpha, 255, 255, 255));
+                    bmp.SetPixel(i, j, Color.FromArgb(alpha, alpha, alpha, alpha));
                 }
             }
 
-            bmp.Save(outputName);
+            return bmp;
         }
 
+        [STAThread]
         static void Main(string[] args)
         {
-            var fontName = "steelfish.ttf";
+            var dlg = new OpenFileDialog { Filter = "Font Files (*.ttf)|*.ttf", InitialDirectory = Directory.GetCurrentDirectory() };
+            if (dlg.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
 
-            Console.WriteLine("Loading "+fontName+"...");
+            var fontFileName = dlg.FileName;
 
-            var bytes = File.ReadAllBytes(fontName);
+            Console.WriteLine($"Loading {fontFileName}...");
+
+            var bytes = File.ReadAllBytes(fontFileName);
 
             var font = new LunarLabs.Fonts.Font(bytes);
 
-            var result = font.RenderGlyph('A', 12);
-            ExportPNG(result.Image, "output.png");
+            int height = 64;
+            var scale = font.ScaleInPixels(height);
+
+            var phrase = "Hellog yorld";
+            var glyphs = new Dictionary<char, FontGlyph>();
+            var bitmaps = new Dictionary<char, Bitmap>();
+            foreach (var ch in phrase)
+            {
+                if (glyphs.ContainsKey(ch))
+                {
+                    continue;
+                }
+
+                var glyph = font.RenderGlyph(ch, scale);
+                glyphs[ch] = glyph;
+                bitmaps[ch] = ConvertToBitmap(glyph.Image);
+            }
+
+            int ascent, descent, lineGap;
+            font.GetFontVMetrics(out ascent, out descent, out lineGap);
+            int baseLine = height - (int)(ascent * scale);
+
+            int outWidth = 0;
+            int realMaxHeight = 0;
+            foreach (var ch in phrase)
+            {
+                var glyph = glyphs[ch];
+                outWidth += glyph.xAdvance;
+
+                int y0 = height - baseLine + glyph.yOfs;
+                int y1 = y0 + glyph.Image.Height;
+                realMaxHeight = Math.Max(realMaxHeight, y1);
+            }
+
+            baseLine += (height - realMaxHeight);
+            int outHeight = realMaxHeight;
+
+            var outBmp = new Bitmap(outWidth, outHeight);
+            using (var g = Graphics.FromImage(outBmp))
+            {
+                // draw the baseline height in blue color
+                var ly = -1 + outHeight - baseLine;
+                g.DrawLine(new Pen(Color.Blue), 0, ly, outWidth - 1, ly);
+
+                // now draw each character
+                int x = 0;
+                foreach (var ch in phrase)
+                {
+                    var glyph = glyphs[ch];
+                    var bmp = bitmaps[ch];
+                    g.DrawImage(bmp, x, height - baseLine + glyph.yOfs);
+
+                    x += glyph.xAdvance;
+                }
+            }
+
+            var outputFileName = "output.png";
+            Console.WriteLine($"Outputting {outputFileName}...");
+            outBmp.Save(outputFileName);
         }
     }
 }
--- README.md
+++ README.md
@@ -10,6 +10,25 @@ The [original C version](https://github.com/nothings/stb/blob/master/stb_truetyp
 
 Since this is a .NET standard package, to use with .NET framework projects please set the target to .NET Framework 4.5 or higher, otherwise Nuget will give you installation errors.
 
+# Sample code
+
+At the core, this library allows loading a font from a file and from there you can rasterize a single character at a specified scale (and also query info about characters).
+Here's a simple example of how to do it:
+
+```c#
+	using LunarLabs.Fonts;
+	//...
+	var bytes = File.ReadAllBytes(fontFileName);
+	var font = new Font(bytes);
+	var scale = font.ScaleInPixels(64);
+	var result = font.RenderGlyph('A', scale);
+	// result.Image now contains the pixels, width and height of the rasterized glyph	
+```
+
+Note that the pixels array stores the pixels as single-channel greyscale data. What you do with this is up to you, you can blit it to the screen, to a bitmap or to an OpenGL texture etc.
+
+You can check the included sample for a more advanced program that shows how to output a full string and save the result as a image file.
+
 # Contact
 
 Let me know if you find bugs or if you have suggestions to improve the code.
