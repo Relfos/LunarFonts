0541c4905b2e5f823c4397c91fdbe0457fab561c
Sergio Flores <relfos@gmail.com>
2019-01-24 14:47:07 +0000

    Added Font Viewer demo

---
+++ FontViewerDemo/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace FontViewerDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.sdfCheckbox = new System.Windows.Forms.CheckBox();
+            this.sdfScaleSelector = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fontHeightSelector = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.PictureBox();
+            this.borderCheckbox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sdfScaleSelector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontHeightSelector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input text";
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(18, 24);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(200, 20);
+            this.textInput.TabIndex = 1;
+            this.textInput.Text = "Hello world!";
+            this.textInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sdfCheckbox
+            // 
+            this.sdfCheckbox.AutoSize = true;
+            this.sdfCheckbox.Location = new System.Drawing.Point(327, 25);
+            this.sdfCheckbox.Name = "sdfCheckbox";
+            this.sdfCheckbox.Size = new System.Drawing.Size(82, 17);
+            this.sdfCheckbox.TabIndex = 2;
+            this.sdfCheckbox.Text = "SDF Output";
+            this.sdfCheckbox.UseVisualStyleBackColor = true;
+            this.sdfCheckbox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // sdfScaleSelector
+            // 
+            this.sdfScaleSelector.Location = new System.Drawing.Point(415, 25);
+            this.sdfScaleSelector.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.sdfScaleSelector.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.sdfScaleSelector.Name = "sdfScaleSelector";
+            this.sdfScaleSelector.Size = new System.Drawing.Size(79, 20);
+            this.sdfScaleSelector.TabIndex = 3;
+            this.sdfScaleSelector.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.sdfScaleSelector.Visible = false;
+            this.sdfScaleSelector.ValueChanged += new System.EventHandler(this.sdfScaleSelector_ValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(18, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 355);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // fontHeightSelector
+            // 
+            this.fontHeightSelector.Location = new System.Drawing.Point(224, 24);
+            this.fontHeightSelector.Maximum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.fontHeightSelector.Minimum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.fontHeightSelector.Name = "fontHeightSelector";
+            this.fontHeightSelector.Size = new System.Drawing.Size(79, 20);
+            this.fontHeightSelector.TabIndex = 5;
+            this.fontHeightSelector.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.fontHeightSelector.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Height";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Change Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(224, 50);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(352, 355);
+            this.outputBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.outputBox.TabIndex = 8;
+            this.outputBox.TabStop = false;
+            // 
+            // borderCheckbox
+            // 
+            this.borderCheckbox.AutoSize = true;
+            this.borderCheckbox.Location = new System.Drawing.Point(224, 411);
+            this.borderCheckbox.Name = "borderCheckbox";
+            this.borderCheckbox.Size = new System.Drawing.Size(57, 17);
+            this.borderCheckbox.TabIndex = 9;
+            this.borderCheckbox.Text = "Border";
+            this.borderCheckbox.UseVisualStyleBackColor = true;
+            this.borderCheckbox.CheckedChanged += new System.EventHandler(this.borderCheckbox_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 440);
+            this.Controls.Add(this.borderCheckbox);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fontHeightSelector);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.sdfScaleSelector);
+            this.Controls.Add(this.sdfCheckbox);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Font Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.sdfScaleSelector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontHeightSelector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.CheckBox sdfCheckbox;
+        private System.Windows.Forms.NumericUpDown sdfScaleSelector;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown fontHeightSelector;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox outputBox;
+        private System.Windows.Forms.CheckBox borderCheckbox;
+    }
+}
+
---
+++ FontViewerDemo/MainForm.cs
@@ -0,0 +1,206 @@
+﻿using LunarLabs.Fonts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FontViewerDemo
+{
+    public partial class MainForm : Form
+    {
+        private string selectedPath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            LoadFontPath(@"C:\Windows\Fonts");
+        }
+
+        static Bitmap ConvertToBitmap(GlyphBitmap image)
+        {
+            var bmp = new Bitmap(image.Width, image.Height);
+            for (int j = 0; j < image.Height; j++)
+            {
+                for (int i = 0; i < image.Width; i++)
+                {
+                    byte alpha = image.Pixels[i + j * image.Width];
+                    bmp.SetPixel(i, j, Color.FromArgb(alpha, 0, 0, 0));
+                }
+            }
+
+            return bmp;
+        }
+
+        static Bitmap GenerateOutput(string fontFileName, string phrase, int pixels, int SDF_scale)
+        {
+            var bytes = File.ReadAllBytes(fontFileName);
+
+            var font = new LunarLabs.Fonts.Font(bytes);
+
+            int height = pixels * SDF_scale;
+            var scale = font.ScaleInPixels(height);
+
+            var glyphs = new Dictionary<char, FontGlyph>();
+            var bitmaps = new Dictionary<char, Bitmap>();
+            foreach (var ch in phrase)
+            {
+                if (glyphs.ContainsKey(ch))
+                {
+                    continue;
+                }
+
+                var glyph = font.RenderGlyph(ch, scale);
+                glyphs[ch] = glyph;
+                bitmaps[ch] = ConvertToBitmap(glyph.Image);
+            }
+
+            int ascent, descent, lineGap;
+            font.GetFontVMetrics(out ascent, out descent, out lineGap);
+            int baseLine = height - (int)(ascent * scale);
+
+
+            int minX = int.MaxValue;
+            int maxX = int.MinValue;
+            int minY = int.MaxValue;
+            int maxY = int.MinValue;
+
+            var positions = new Point[phrase.Length];
+
+            int x = 0;
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                var ch = phrase[i];
+                var glyph = glyphs[ch];
+
+                var next = i < phrase.Length - 1 ? phrase[i + 1] : '\0';
+
+                var kerning = font.GetKerning(ch, next, scale);
+
+                int y0 = height - baseLine + glyph.yOfs;
+                int y1 = y0 + glyph.Image.Height;
+
+                int x0 = x + glyph.xOfs - kerning;
+                int x1 = x0 + glyph.Image.Width;
+                x += glyph.xAdvance;
+
+                positions[i] = new Point(x0, y0);
+
+                x1 = Math.Max(x, x1);
+
+                minX = Math.Min(minX, x0);
+                maxX = Math.Max(maxX, x1);
+
+                minY = Math.Min(minY, y0);
+                maxY = Math.Max(maxY, y1);
+            }
+
+            int realWidth = (maxX - minX) + 1;
+            int realHeight = (maxY - minY) + 1;
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                positions[i].X -= minX;
+                positions[i].Y -= minY;
+            }
+
+            var tempBmp = new GlyphBitmap(realWidth, realHeight);
+            {
+                // draw the baseline height in blue color
+                var ly = height - (baseLine + minY);
+                //g.DrawLine(new Pen(Color.Blue), 0, ly, realWidth - 1, ly);
+
+                // now draw each character
+                x = 0;
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    var ch = phrase[i];
+                    var glyph = glyphs[ch];
+                    var bmp = bitmaps[ch];
+                    var pos = positions[i];
+                    tempBmp.Draw(glyph.Image, pos.X, pos.Y);
+                }
+            }
+
+            if (SDF_scale > 1)
+            {
+                tempBmp = DistanceFieldUtils.CreateDistanceField(tempBmp, SDF_scale, 16 * SDF_scale);
+            }
+
+            return ConvertToBitmap(tempBmp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath)) // check result.
+            {
+                LoadFontPath(dialog.SelectedPath);
+            }
+        }
+
+        private void LoadFontPath(string selectedPath)
+        {
+            this.selectedPath = selectedPath;
+
+            var files = Directory.GetFiles(selectedPath, "*.ttf");
+            listBox1.Items.Clear();
+            foreach (var file in files)
+            {
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(file));
+            }
+
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            sdfScaleSelector.Visible = sdfCheckbox.Checked;
+            RefreshOutput();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty( textInput.Text))
+            {
+                return;
+            }
+
+            RefreshOutput();
+        }
+
+        private void RefreshOutput()
+        {
+            var fontFileName = selectedPath + @"\" + listBox1.SelectedItem + ".ttf";
+            var sdfScale = sdfCheckbox.Checked ? (int)sdfScaleSelector.Value : 1;
+            outputBox.Image = GenerateOutput(fontFileName, textInput.Text, (int)fontHeightSelector.Value, sdfScale);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshOutput();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshOutput();
+        }
+
+        private void sdfScaleSelector_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshOutput();
+        }
+
+        private void borderCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            outputBox.BorderStyle = borderCheckbox.Checked ? BorderStyle.FixedSingle : BorderStyle.None;
+        }
+    }
+}
